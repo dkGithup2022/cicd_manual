6476cb5dbfda9f1f1430ba7594c56f6aef863661
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-01-19 01:49:25 +0900

    webhook test

--- src/main/java/io/github/giggalsPeople/lib/AnyLibrary.java
+++ src/main/java/io/github/giggalsPeople/lib/AnyLibrary.java
@@ -3,7 +3,9 @@ package io.github.giggalsPeople.lib;
 
 public class AnyLibrary {
     private static final String HW = "HELLO WORLD";
-    public static String helloWorld(){
+
+    public static String helloWorld() {
+        System.out.println("changed");
         return HW;
     }
 }
