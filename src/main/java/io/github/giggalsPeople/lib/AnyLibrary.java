9ec456e6ea99a17593ec54e7c3dec80857b1f8c7
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-01-19 02:11:45 +0900

    Update AnyLibrary.java

--- src/main/java/io/github/giggalsPeople/lib/AnyLibrary.java
+++ src/main/java/io/github/giggalsPeople/lib/AnyLibrary.java
@@ -5,7 +5,7 @@ public class AnyLibrary {
     private static final String HW = "HELLO WORLD";
 
     public static String helloWorld() {
-        System.out.println("changed2");
+        System.out.println("changed3");
         return HW;
     }
 }
